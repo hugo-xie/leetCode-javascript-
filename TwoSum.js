fe543cf6b56dc0d9bc5620c83a6c4ecad04f8741
hugo-xie <m15955038579@163.com>
2017-11-19 09:09:36 +0800

    two sum javascript

---
+++ TwoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+//暴力法
+var twoSum = function(nums, target) {
+    var result = [];
+    for(var i = 0; i<nums.length;i++){
+        for(var j = i+1;j<nums.length;j++){
+            if(nums[i]+nums[j] === target){
+                result.push(i);
+                result.push(j);
+            }
+        }
+    }
+    return result;
+};
